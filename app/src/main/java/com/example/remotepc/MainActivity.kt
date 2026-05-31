49cfbe4e8c54b1f62ca8ee7f081583435e52bf6b
Ihor Zaletskiy <igor.zaletsky@fasunok.com>
2026-05-31 14:53:57 +0300

    Update layout attributes for ScrollView and modify main card title for clarity

--- app/src/main/java/com/example/remotepc/MainActivity.kt
+++ app/src/main/java/com/example/remotepc/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.example.remotepc
 
-import android.content.Context
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
